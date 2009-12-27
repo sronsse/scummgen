b678482eddfdd09c83d9ef2b4117f8d80f84955b
sronsse <sronsse@bb329ef9-dc39-0410-9a3b-5b3e18aa4ead>
2009-12-27 20:10:07 +0000

    Fixed an overflow issue linked to object coordinates.

--- trunk/src/blocks/CDHD.hpp
+++ trunk/src/blocks/CDHD.hpp
@@ -13,14 +13,12 @@ private:
 	static const uint16_t UNKNOWN_1;
 	static const uint16_t UNKNOWN_2;
 	uint16_t _id;
-	uint8_t _x;
-	uint8_t _y;
-	uint8_t _width;
-	uint8_t _height;
+	uint16_t _x;
+	uint16_t _y;
+	uint16_t _width;
+	uint16_t _height;
 	uint8_t _flags;
 	uint8_t _parent;
-	uint16_t _walkX;
-	uint16_t _walkY;
 	uint8_t _actorDir;
 public:
 	CDHD(Object *object);
@@ -30,4 +28,3 @@ public:
 };
 
 #endif
-
